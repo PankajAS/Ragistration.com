ffa93bc9ec987e1b21fbb175eaeb5d67fb9b8fbc
Pankaj <sutharpankaj91@gmail.com>
2016-10-29 16:28:13 +0530

    Update MainController.js

--- js/MainController.js
+++ js/MainController.js
@@ -2,9 +2,7 @@ app.controller("MainController",function($http, $scope, $interval, $location){
     
     $scope.search = function(username) {
       console.log("going to new page: " + username)
-      $location.path("/user/" + username)
-    
-	  
+      $location.path("/user/" + username)   
     }
        
     $scope.username = "pankajas";
